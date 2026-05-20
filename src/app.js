92df58838a3be11696368d3b0fc730d8cf3d80fb
Justice Inkoom <justiceinkoom55@gmail.com>
2026-05-20 19:34:18 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ function login() {
     return;
   }
 
-  if (pin === String(settings.pin)) {
+  if (pin === settings.pin) {
     document.getElementById('loginScreen').classList.add('hidden');
     document.getElementById('app').classList.remove('hidden');
     clearLoginError();
